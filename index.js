0e5d27368c7954b0df8b933450602f65e1b3d80d
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2024-02-18 23:57:58 -0300

    Adding initial project

---
+++ index.js
@@ -0,0 +1,39 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-west-2",
+  accessKeyId: "accessKeyId",
+  secretAccessKey: "secretAccessKey",
+  endpoint: "http://localhost:8000",
+});
+
+const dynamoDb = new AWS.DynamoDB();
+const params = {
+  TableName: "BaseballStats",
+  KeySchema: [
+    { AttributeName: "TeamId", KeyType: "HASH" },
+    { AttributeName: "SK", KeyType: "RANGE" },
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "TeamId", AttributeType: "S" },
+    { AttributeName: "SK", AttributeType: "S" },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5,
+  },
+};
+
+dynamoDb.createTable(params, (err, data) => {
+  if (err) {
+    console.error(
+      "Unable to create table. Error JSON:",
+      JSON.stringify(err, null, 2)
+    );
+  } else {
+    console.log(
+      "Created table. Table description JSON:",
+      JSON.stringify(data, null, 2)
+    );
+  }
+});
